067c5e4166748272b9da222036dd06c4fa2699a0
bbbbelle <bellthanyared@email.com>
2023-11-28 16:59:08 +0700

    Lab03

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import datetime
+from flask import jsonify, render_template
 from flask import jsonify
 from app import app
 from app import hw_views
@@ -17,7 +19,32 @@ def data():
         "Alice": "(708) 727-2377",
         "Bob": "(305) 734-0429"
     }
+    app.logger.debug(str(len(d)) + " entries in phonebook")
+
     return jsonify(d)  # convert your data to JSON and return
 @app.route('/lab02')
 def resume():
     return app.send_static_file('lab02_resume.html')
+
+@app.route('/crash')
+def crash():
+    return 1/0
+
+@app.route('/lab03')
+def lab03_home():
+    return render_template('lab03/index.html',
+                           utc_dt=datetime.datetime.utcnow())
+
+@app.route('/lab03/about/')
+def lab03_about():
+    return render_template('lab03/about.html')
+
+@app.route('/lab03/comments/')
+def lab03_comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.']
+
+
+    return render_template('lab03/comments.html', comments=comments)
