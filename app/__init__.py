a356be8bdb864557f772622c04d3d73602889d1a
bbbbelle <bellthanyared@email.com>
2023-12-19 15:39:13 +0700

    Lab05

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__, static_folder='static')
-
+app.url_map.strict_slashes = False
 app.jinja_options = app.jinja_options.copy()
 app.jinja_options.update({
     'trim_blocks': True,
@@ -10,5 +10,8 @@ app.jinja_options.update({
 
 
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = \
+    '116333cef87fdb07af8e7a976ec8666a61bd7d7d75afbb32'
+
 
 from app import views # noqa
