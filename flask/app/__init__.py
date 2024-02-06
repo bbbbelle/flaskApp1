5a218eda3e39bcb4a4a3560166d41d290b4bf2b5
bbbbelle <bellthanyared@email.com>
2024-02-06 16:12:24 +0700

    debug mode setup

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,11 +1,10 @@
 import os
 from flask import Flask
+from werkzeug.debug import DebuggedApplication
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__, static_folder='static')
 app.url_map.strict_slashes = False
-
 
 app.jinja_options = app.jinja_options.copy()
 app.jinja_options.update({
@@ -13,21 +12,18 @@ app.jinja_options.update({
     'lstrip_blocks': True
 })
 
-
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = \
     'b1859c806f35f87b8beb87bb27fd6dffe7ec382f3c68c5b3'
-
 app.config['JSON_AS_ASCII'] = False
-
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+if app.debug:
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
-
 
 from app import views  # noqa
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,7 +1,6 @@
-import json
 from flask import (jsonify, render_template,
                    request, url_for, flash, redirect)
-
+import json
 from sqlalchemy.sql import text
 from app import app
 from app import db
@@ -12,18 +11,6 @@ def home():
     return "Flask says 'Hello world!'"
 
 
-@app.route('/db')
-def db_connection():
-    try:
-        with db.engine.connect() as conn:
-            conn.execute(text("SELECT 1"))
-        return '<h1>db works.</h1>'
-    except Exception as e:
-        return '<h1>db is broken.</h1>' + str(e)
-
-
-@app.route('/lab04')
-def lab04_bootstrap():
-    return app.send_static_file('lab04_bootstrap.html')
-
-
+@app.route('/crash')
+def crash():
+    return 1/0
