46ac428fdcf6c51afb449330fe37d2f73d6d961b
bbbbelle <bellthanyared@email.com>
2024-02-06 16:04:50 +0700

    database setup

--- flask/gunicorn_starter.sh
+++ flask/gunicorn_starter.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+if [ "$APP_ENV" = "development" ]; then
+    echo "Creating the database tables..."
+    python3 manage.py create_db
+    python3 manage.py seed_db
+    echo "Tables created"
 
-# gunicorn main:app -c "$PWD"/gunicorn.config.py
-# exec "$@"
-if [ "$FLASK_DEBUG" = "1" ]; then
-    echo "Running on Flask Development Server"
-    python3 main.py
+    if [ "$FLASK_DEBUG" = "1" ]; then
+        echo "Running on Flask Development Server"
+        python3 main.py
+    else
+        echo "Running on gunicorn"
+        gunicorn main:app -c "$PWD"/gunicorn.config.py
+    fi
 else
     echo "Running on gunicorn"
     gunicorn main:app -c "$PWD"/gunicorn.config.py
